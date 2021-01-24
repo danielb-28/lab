9e1e961cf58a3a6a1a8be8cbe814d027c8f6f6e9
danielb <danielb@danielb>
2021-01-24 20:25:13 -0300

    20210121

--- GUI/QT_ADC_1/mainwindow.cpp
+++ GUI/QT_ADC_1/mainwindow.cpp
@@ -11,27 +11,17 @@ boost::asio::serial_port mcu(io); // Porta
 
 QTimer timer; // Timer update
 
-uint8_t comando; // Comando serial
+uint16_t comando; // Comando serial
 
-bool primeira_exec2 = true; // Controle do update - NAO ESTA FUNCIONANDO DA FORMA ESPERADA
+bool primeira_exec2 = true; // Controle do update // MOD
 
 int x_max = 0; // Valor maximo de amostras
+
+bool inverter = false; // Inverter plot
 
 // Tempo para monitorar o fps
 auto t1 = std::chrono::steady_clock::now();
 auto t2 = std::chrono::steady_clock::now();
-
-// Utilizar na mbed
-/*
-uint num_amostras()
-{
-    uint valor = (comando>>2) & 0x07;
-
-    if (valor==0x00 || valor==0x01) return 50 * (1 + valor);
-    if (valor==0x02 || valor==0x03) return 250 * (1 + (valor - 2));
-    else return 1000 * (valor - 3);
-}
-*/
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -141,7 +131,6 @@ void MainWindow::serial_open()
     QString dev_str = "/dev/"; // Prefixo
     porta = dev_str.append(porta); // Forma a string da porta
     mcu.open(porta.toUtf8().constData()); // Abre a porta
-    //mcu.open("/dev/ttyACM0"); // Abre a porta
     mcu.set_option(boost::asio::serial_port_base::baud_rate(BAUD)); // Configura a baud rate
 
     ui->label_status->setText("Conectado / " + porta); // Indica a porta aberta
@@ -155,6 +144,7 @@ void MainWindow::serial_config()
 
     int clock = ui->comboBox_clock->currentIndex(); // Get clock selecionado
     int amostras = ui->comboBox_amostras->currentIndex(); // Get amostra selecionada
+    bool dac_sinal = ui->comboBox_sinaldac->currentIndex();
 
     ui->label_amostras->setText(ui->comboBox_amostras->currentText()); // Indica o numero de amostras
     ui->label_clock->setText(ui->comboBox_clock->currentText()); // Indica o clock
@@ -165,120 +155,87 @@ void MainWindow::serial_config()
     comando = 0x02; // Trigger externo desativado
     comando |= (amostras << 2);
     comando |= (clock << 5);
+    comando |= (dac_sinal << 8);
+
+    qInfo() << (uint16_t) comando; // DEBUG
 
 }
 
 // Envia o comando e recebe os dados
 void MainWindow::serial_start(){
 
+    uint16_t i_label; // Label inteiro
+
+    std::string s_label; // String de label
+
     std::string dados; // String de dados recebidos
 
-    boost::asio::write(mcu, boost::asio::buffer(&comando, 8)); // Comando para aquisição
+    boost::asio::write(mcu, boost::asio::buffer(&comando, 16)); // Comando para aquisição
 
-    //boost::asio::read_until(mcu, boost::asio::dynamic_buffer(dados), 'E'); // Leitura dos dados
+    boost::asio::read(mcu, boost::asio::dynamic_buffer(s_label, 2)); // Label do pacote de dados
 
-    // TESTE
+    i_label = (uint16_t)((s_label[0] << 8) + (s_label[1] & 0x00FF));
 
-    boost::asio::read(mcu, boost::asio::dynamic_buffer(dados, 2*x_max)); // TESTE
+    x_max = (i_label >> 4);
 
-    uint16_t dado_conv;
+    boost::asio::read(mcu, boost::asio::dynamic_buffer(dados, 2*(i_label >> 4)));
 
-    this->x_data.clear(); // MOD
-    this->y_data.clear(); // MOD
-    //qInfo() << dados_str_teste.length(); // DEBUG
-
-    //qInfo() << dados.length(); // DEBUG
-
-    for(int index=0; index<2*x_max; index+=2)
-    {
-        //dado_conv = 0x0000;
-        //qInfo() << ((uint16_t) dados[index] << 8) ; // DEBUG
-        //dado_conv = (dados[i] << 8);
-        //qInfo() << (uint8_t) dados[i+1]; // DEBUG
-        //qInfo() << dado_conv; // DEBUG
-        //dado_conv &= 0xF00;
-
-        //aux1 = ((uint16_t) dados[index] << 8);
-        //aux2 = (uint16_t) dados[index+1] >> 8 ;
-
-        //dado_conv = aux2 | aux1;
-
-        //dado_conv =  dados[index+1] | (uint16_t) dados[index] << 8;
-        //dado_conv &= 0x0FFF;
-
-        //dado_conv = (uint8_t) dados[i+1];
-
-        //memcpy(&dado_conv, &dados[index], 2);
-        dado_conv = (uint16_t)((dados[index] << 8) + (dados[index+1] & 0x00FF));
-
-        //qInfo() << dado_conv; // DEBUG
-
-        //sub_dado = dados.substr(i, i+1);
-
-        //qInfo() << sub_dado[0] << sub_dado[1];
-
-        this->x_data.append((double) index/2);
-        this->y_data.append((double) dado_conv);
-    }
-
-    // ======
-
-    //convert_dados(dados); // Dados de string para double
+    convert_dados(dados);
 
 }
 
 // Conversao e processamento dos dados
-void MainWindow::convert_dados(std::string dados){
+void MainWindow::convert_dados(std::string dados)
+{
 
-    int j = 0;
-    int inicio = 0;
-    int fim = 1;
+    uint16_t dado_conv;
 
-    int i = 0;
+    uint16_t dado_ant = 0; // MOD
 
-    int dado_double = 0;
+    float alfa = this->ui->doubleSpinBox->value(); // TEST
+    inverter = this->ui->checkBox->checkState(); // TEST
 
-    if (primeira_exec2){
+    if (primeira_exec2)
+    {
         this->x_data.clear();
         this->y_data.clear();
     }
 
-    // Conversao da string retornada para um array double
-    for (i = 0; i < (int) dados.size(); i++)
+    for(int index=0; index<2*x_max; index+=2)
     {
+        dado_conv = (uint16_t)((dados[index] << 8) + (dados[index+1] & 0x00FF));
 
-        // Final de uma string especifica
-        if (dados[i] == ',')
+        // Filtro
+        if(index>0)
         {
 
-            if (primeira_exec2){
-                dado_double = boost::lexical_cast<double>(dados.substr(inicio, fim - inicio - 1)); // Conversao da string
-                dado_double *= -1;
-                dado_double += 4096;
-                this->x_data.append((double) j);
-                this->y_data.append(dado_double);
-            }
-
-            else{
-                dado_double = boost::lexical_cast<double>(dados.substr(inicio, fim - inicio - 1));
-                dado_double *= -1;
-                this->x_data.replace(j, (double) j);
-                this->y_data.replace(j, dado_double);
-            }
-
-            inicio = i + 1; // Redefine o inicio da string para a proxima string
-            fim = inicio + 1; // Redefine o final da string
-
-            j++; // Incrementa o indice do array double
+            dado_conv = dado_ant + alfa * (dado_conv - dado_ant);
 
         }
 
-        // Continuacao da leitura
+        if (primeira_exec2){
+            if(inverter==true){
+                this->y_data.append(4096 - (double) dado_conv);
+            }
+            else{
+                this->y_data.append((double) dado_conv);
+            }
+            this->x_data.append((double) index/2);
+        }
+
         else
         {
-            if (dados[i] == 'E') break; // Verifica se chegou ao fim
-            else fim++; // Incrementa o fim da string e continua a leitura
-        }
+
+            if(inverter==true){
+                this->y_data.replace(index/2, 4096 - (double) dado_conv);
+            }
+            else{
+                this->y_data.replace(index/2, (double) dado_conv);
+            }
+            this->x_data.replace(index/2, (double) index/2);
+         }
+
+        dado_ant = dado_conv;
 
     }
 
@@ -295,10 +252,9 @@ void MainWindow::plot(){
         ui->plot_widget->graph(0)->setData(this->x_data, this->y_data);
         ui->plot_widget->xAxis->setRange(0, x_max);
         ui->plot_widget->yAxis->setRange(0, 4096);
-        //ui->plot_widget->yAxis->setRange(0, 255); // TEST
 
         // Configuracoes graficas
-        ui->plot_widget->graph(0)->setAntialiased(false);
+        ui->plot_widget->graph(0)->setAntialiased(true);
         ui->plot_widget->graph(0)->setAdaptiveSampling(true);
 
     }
