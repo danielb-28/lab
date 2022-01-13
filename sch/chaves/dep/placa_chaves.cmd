2bbb7e2e94416864cb0009cef661c3677234e36e
danielb <danielb@danielb>
2022-01-13 17:39:36 -0300

    20220113

---
+++ sch/chaves/dep/placa_chaves.cmd
@@ -0,0 +1,91 @@
+# Pin name action command file
+
+# Start of element U2
+ChangePinName(U2, 8, 8)
+ChangePinName(U2, 7, 7)
+ChangePinName(U2, 6, 6)
+ChangePinName(U2, 5, 5)
+ChangePinName(U2, 1, 1)
+ChangePinName(U2, 2, 2)
+ChangePinName(U2, 4, 4)
+ChangePinName(U2, 3, 3)
+
+# Start of element U1
+ChangePinName(U1, 8, 8)
+ChangePinName(U1, 7, 7)
+ChangePinName(U1, 6, 6)
+ChangePinName(U1, 5, 5)
+ChangePinName(U1, 1, 1)
+ChangePinName(U1, 2, 2)
+ChangePinName(U1, 4, 4)
+ChangePinName(U1, 3, 3)
+
+# Start of element J8
+ChangePinName(J8, 1, 1)
+
+# Start of element J7
+ChangePinName(J7, 1, 1)
+
+# Start of element J6
+ChangePinName(J6, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element J5
+ChangePinName(J5, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element J10
+ChangePinName(J10, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element J4
+ChangePinName(J4, 1, 1)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element J9
+ChangePinName(J9, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
