62a26de4ea8c84afab73d87e92c8c2f4b6adf685
danielb-28 <danielb@danielb>
2022-05-31 17:09:09 -0300

    socket_can

---
+++ misc/socket_can/main.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <net/if.h>
+#include <string.h>
+
+#define CAN_NAME "vcan1"
+
+int main(int argc, char **argv){
+
+	int fd; // file_desc
+	struct ifreq can_ifreq; // ifreq
+	struct can_frame dado; // data frame
+	struct sockaddr_can can_addr; // endereco
+
+	// file_desc
+	if ((fd = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) // Retorna o file_desc 
+	{
+		perror("Erro na criacao do socket");
+		return 1;
+	}
+	puts("Socket criado");
+
+	// ifr_name
+	strcpy(can_ifreq.ifr_name, CAN_NAME);
+
+	// indice da interface
+	ioctl(fd, SIOCGIFINDEX, &can_ifreq); // retorna o indice em can_ifreq.ifr_ifindex
+	
+	// addr
+	memset(&can_addr, 0, sizeof(can_addr));
+	can_addr.can_family = AF_CAN;
+	can_addr.can_ifindex = can_ifreq.ifr_ifindex
+
+	// bind
+	if (bind(fd, (struct sockaddr *)&can_addr, sizeof(can_addr)) < 0) {
+		perror("Erro no bind");
+		return 1;
+	}
+	puts("Bind feito");
+
+	// Envio dos dados
+	data.can_id = 0x001;
+	sprintf(data.data, 0x001);
+
+	if (write(fd, &data, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
+		perror("Erro no envio dos dados");
+		return 1;
+	}
+	puts("Dados enviados");
+
+	// Fecha o socket
+	if (close(fd) < 0) {
+		perror("Erro ao fechar o socket");
+		return 1;
+	}
+
+	return 0;
+}
